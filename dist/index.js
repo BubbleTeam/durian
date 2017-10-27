4995a814dab3f4558dce28f0c80c4abbfab0b76b
hzfangwentian <hzfangwentian@corp.netease.com>
2017-10-27 22:24:51 +0800

    update

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,17 @@
 'use strict';
 
-console.log(12313);
+var minify = {
+    
+};
+
+var fs = require('fs');
+var index = {
+    parse: function parse(jsonString) {
+        return JSON.parse(minify(jsonString));
+    },
+    minify: function minify$1(jsonString) {
+        return minify(jsonString);
+    }
+};
+
+module.exports = index;
--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,14 @@
+import resolve from 'rollup-plugin-node-resolve';
+import buble from 'rollup-plugin-buble';
+
 export default {
     input: './index.js',
     output: {
         file: './dist/index.js',
         format: 'cjs'
-    }  
+    },
+    plugins: [
+        buble(),
+        resolve()
+    ]
 }
