4bfd43c7a3c2fea4aecdaa4aad577e7555988035
hzfangwentian <hzfangwentian@corp.netease.com>
2017-10-27 14:12:54 +0800

    update

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log(12313);
+const fs = require('fs');
+import minify from './lib/minify';
+
+export default {
+    parse(jsonString) {
+        return JSON.parse(minify(jsonString));
+    },
+    minify(jsonString) {
+        return minify(jsonString);
+    }
+}
